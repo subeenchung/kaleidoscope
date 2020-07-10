0b0456acb0e910e70ddc9a4ea0686c84ae7eba23
Subeen Chung <chungsubeen@gmail.com>
2020-07-10 02:13:34 -0400

    test

--- main.go
+++ main.go
@@ -15,16 +15,16 @@ import (
 
 func main() {
 
-	//Load TOML rule configuration
+	// Load TOML rule configuration
 	m, err := filepath.Glob("./windows/*.toml")
 	if err != nil {
 		log.Fatalf("could not load toml rules: %v\n", err)
 	}
-
+	// Stores Windows rulesets
 	wrules := &types.Ruleset{}
 
 	for _, elem := range m {
-		wf := &types.WindowsRulesFormat{}
+		wf := &types.WindowsRuleFormat{}
 		b, err := ioutil.ReadFile(elem)
 		if err != nil {
 			log.Fatalf("could not read file: %v err: \n", elem, err)
@@ -33,11 +33,11 @@ func main() {
 		if err != nil {
 			log.Fatalf("err occurred while unmarshalling toml: %v\n", err)
 		}
-		append(&wf.Rules, wf)
+		wrules.Rules = append(wrules.Rules, wf)
 
 	}
 
-	for _, elem := range wf.Rules {
+	for _, elem := range wrules.Rules {
 		fmt.Printf("%v\n", elem)
 	}
 
@@ -54,9 +54,11 @@ func main() {
 	c.SubscribeTopics([]string{"winlogbeat"}, nil)
 
 	for {
+		// read message until killed
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
 			wl := &types.Winlogbeat{}
+			// je - json unmarshaling error message
 			je := json.Unmarshal(msg.Value, &wl)
 			if je != nil {
 				log.Fatalf("could not unmarshal incoming message: %v\nmessage body: %v\n", je, string(msg.Value))
